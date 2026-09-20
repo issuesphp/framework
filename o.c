58863be22dade34d7465ad81cd2153686ca1ac4a
jonathan <programadorjonathan@gmail.com>
2026-09-20 08:11:19 -0400

    update

--- o.c
+++ o.c
@@ -5,41 +5,68 @@
 
 #include <stdlib.h>
 
-// esta es funcion principal
-int main() {
+#include <iostream>
 
-	// const int num = 7;
+int main() {
 
 	int myNum;
 
-	// num = 10;
+	char option[50];
+
+	std::string base = "base_table.php";
+
+
+	// strcpy(option, "Options");
+
 
 	printf("Choice Option: \n");
 
-// Get and save the number the user types
 	scanf("%d", &myNum);
+
+	// scanf("%s", &option);
+
+	//leemos 48 characters
+	// scanf("%49s", option);
 
 // Output the number the user typed
 	// printf("Your number is: %d", myNum);
 
 	switch (myNum) {
+		// no se puede usar string con switch 
+	// case 'issuesphp:serve':
 	case 1:
 		// printf("Your number is: %d\n", myNum);
 
-		FILE *archivo = fopen("test.php", "w");
+		system("php -S localhost:8080");
+
 		break;
 	case 2:
-		printf("Your number is: %d\n", myNum);
+
+		printf("Choice Option: \n");
+
+		scanf("%49s", option);
+
+		if (strcmp(option, "customers") == 0)
+		{
+			// std::string base = "base_table.php";
+		// std::string endpoint = baseUrl + segment;	
+
+		FILE *file = fopen("database/migrations/base_table.php", "w");		
+
+		// FILE *file = fopen("database/migrations/" + option.c_str() + "_table.php", "w");
+
+			// FILE *file = fopen("database/migrations/" + base, "w");
+		}
+
+
+
 		break;
 	default:
-		printf("Nro incorrecto");
-	}
+		printf("Choose an option from the list");
 
-
+		return -1;
+	}	
 	
-
-	
-	return 0;
 
 	
 	return 0;
